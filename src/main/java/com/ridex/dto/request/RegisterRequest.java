d617723d9eb3cd75a97e51a4a0c761c1024e7af6
abdullahisdev <abdullahisdev@gmail.com>
2026-04-01 19:56:11 +0500

    Add a dto request named as 'RegisterRequest'

---
+++ src/main/java/com/ridex/dto/request/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.ridex.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+
+public class RegisterRequest {
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String phone;
+}
